328296665fdaa1e5e72024864c7756788fb35dcf
Stefan Partin <stefanjp@gmail.com>
2019-01-13 08:23:39 -0800

    added create donation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor 
 
 app = Flask(__name__)
 
@@ -15,6 +15,24 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    ''' Create a new donation from user input of donor name and donation amount '''
+    if request.method == 'POST':
+        try:
+            donor = Donor.select().where(Donor.name == request.form['name']).get()
+        except Donor.DoesNotExist:
+            new_donor = Donor(name=request.form['name'])
+            new_donor.save()
+            
+            Donation(value=request.form['amount'],donor=new_donor).save()
+        else:
+            Donation(value=request.form['amount'], donor=donor).save()
+
+        return redirect(url_for('all'))
+
+    return render_template('create.jinja2')
     
 
 if __name__ == "__main__":
